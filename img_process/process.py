e91039b464d11a1504fab502c60057edf168b28f
Rajitha-Manellanga <rajitha.mail48@gmail.com>
2022-05-04 13:54:53 +0300

    [34] Rajitha - Updated image pocessing algorithm

--- img_process/process.py
+++ img_process/process.py
@@ -14,18 +14,32 @@ class ImageProcess():
         mask = np.ones(img.shape[:2], np.uint8)
         img_gray =  cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img_gray = cv2.bitwise_and(img_gray,img_gray, mask=mask)
-        ret,thresh = cv2.threshold(img_gray, 128, 255,cv2.THRESH_OTSU|cv2.THRESH_BINARY)
-        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (45,45))
-        dilation = cv2.dilate(thresh,rect_kernel, iterations = 1)
-        self.draw_contours(dilation,img_name)
+        #binary and inv threshold to select the best one
+        binary_ret,binary_thresh = cv2.threshold(img_gray, 180, 255,cv2.THRESH_OTSU+cv2.THRESH_BINARY)
+        binary_inv_ret,binary_inv_thresh = cv2.threshold(img_gray, 180, 255,cv2.THRESH_OTSU+cv2.THRESH_BINARY_INV)
+        #morph rectangle
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (35,35))
+        #dialte images
+        binary_dilation = cv2.dilate(binary_thresh,rect_kernel, iterations = 1)
+        binary_inv_dilation = cv2.dilate(binary_inv_thresh,rect_kernel, iterations = 1)
+        self.draw_contours(binary_dilation,binary_inv_dilation,img_name)
     
-    def draw_contours(self,dilation_img,img_name):
-        contours, hierarchy = cv2.findContours(dilation_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    def draw_contours(self,binary_dilation_img,binary_inv_dilation_img,img_name):
+        binary_contours, binary_hierarchy = cv2.findContours(binary_dilation_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        binary_inv_contours, binary_inv_hierarchy = cv2.findContours(binary_inv_dilation_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+       
         output = {}
         font = cv2.FONT_HERSHEY_SIMPLEX
         img_list = []
         img2 = self.img.copy()
         num = 0
+
+        #check for the most suitable threshold contours type
+        if len(binary_contours) >= len(binary_inv_contours):
+            contours = binary_contours
+        else:
+            contours = binary_inv_contours
+
         for cnt in contours:
             if cv2.contourArea(cnt) > 0:
                 x, y, w, h = cv2.boundingRect(cnt)
@@ -38,6 +52,8 @@ class ImageProcess():
                 img_arr["height"] = h
 
                 colors = detect.ColorDetector(ROI).detect_colors()
+                print(num)
+                print(colors)
                 colors = sorted(colors)
                 color_1 = colors[0][0]
                 color_2 = colors[-1][0]
