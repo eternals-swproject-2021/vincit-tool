ba74e3cb601fbe09e4f9ec4155b2556c827ac486
rakkant <rakkantia@gmail.com>
2022-05-04 14:14:52 +0300

    [19] Rakkan - Created loading in progress page and spinner

--- react-app/src/pages/services/Services.js
+++ react-app/src/pages/services/Services.js
@@ -1,6 +1,7 @@
 import { React, useState, Component } from 'react'
 import FileUpload from '../../components/fileUpload/FileUpload';
 import FileList from '../../components/fileList/FileList';
+import LoadingSpinner from '../../components/loadingPage/LoadingSpinner';
 import './services.css';
 
 const Services = () => {
@@ -9,6 +10,44 @@ const Services = () => {
     const removeFile = (filename) => {
         setFiles(files.filter(file => file.name !== filename))
     }
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleFetch = () => {
+        setIsLoading(true);
+        fetch("https://color-contrast.free.beeceptor.com")
+            .then((respose) => respose.json())
+            .then((respose) => {
+                setUsers(respose.data)
+                setIsLoading(false)
+                // Optional code to simulate delay
+                setTimeout(() => {
+                  setUsers(respose.data);
+                  setIsLoading(false);
+                }, 3000);
+            })
+            .catch(() => {
+                // setErrorMessage("Unable to fetch user list");
+                setIsLoading(true);
+            });
+    };
+
+    const renderUser = (
+        <div className="userlist-container">
+            {users.map((item, index) => (
+                <div className="user-container" key={index}>
+                    <img src={item.avatar} alt="" />
+                    <div className="userDetail">
+                        <div className="first-name">{`${item.first_name}                
+                                       ${item.last_name}`}</div>
+                        <div className="last-name">{item.email}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 
     return (
         <div>
@@ -21,8 +60,10 @@ const Services = () => {
             <div>
                 {files.length === 0
                     ? <input className="evaluate-btn" type="submit" value="Evaluate" disabled="true" />
-                    : <input className="evaluate-btn" type="submit" value="Evaluate" />}
+                    : <input className="evaluate-btn" type="submit" value="Evaluate" onClick={handleFetch} disabled={isLoading}/>}
             </div>
+            {isLoading ? <LoadingSpinner /> : renderUser}
+            {errorMessage && <div className="error">{errorMessage}</div>}
         </div>
     );
 }
