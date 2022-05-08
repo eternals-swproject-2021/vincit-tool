c0b8744a45314e1bbab1fd83a4f88644ab45480f
rakkant <rakkantia@gmail.com>
2022-05-08 13:51:45 +0300

    [30] Rakkan - Removed unused css and fixed padding

--- react-app/src/pages/document/Document.js
+++ react-app/src/pages/document/Document.js
@@ -10,7 +10,7 @@ const Document = () => {
         <div>
             <h1 className="headline">Document</h1>
             <div class="container col-xxl-8 px-4 py-5">
-                <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="row flex-lg-row-reverse align-items-center g-5 py-4">
                     <div class="col-10 col-sm-8 col-lg-6">
                         <img src={W3C} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"></img>
                     </div>
