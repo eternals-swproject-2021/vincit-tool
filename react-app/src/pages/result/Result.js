604e217b661197f00fde1fe679d85ddbea8e0947
rakkant <rakkantia@gmail.com>
2022-05-13 15:35:18 +0300

    [17] Rakkan - Added ScrollButton to result page and able to display output result (except pic)

--- react-app/src/pages/result/Result.js
+++ react-app/src/pages/result/Result.js
@@ -1,59 +1,93 @@
-import React, { Component } from "react";
+import React from "react";
 import LoadingSpinner from "../../components/loadingPage/LoadingSpinnerEvaluate";
+import { BiCheck } from "react-icons/bi"
+import { RiCloseFill } from "react-icons/ri"
+import ScrollButton from "../../components/scrollButton/ScrollButton";
 // import weblogo from './contours/web-logo.png';
 import "./result.css";
 
+export default function Result(props) {
 
-export default class Result extends Component {
-    render() {
+    // const [id, setID] = useState('');
 
-        const subEvaluation = JSON.stringify(this.props.displayedResults.sub_evaluation);
-        console.log(subEvaluation);
-
-
-        if (this.props.displayedResults.sub_evaluation === undefined) {
-            console.log("is loading")
-            return <div><LoadingSpinner /></div>
-        }
-        return (
-            <div>
-                <h2 className="headline">Evaluation Result</h2>
-                <div class="text-center">
-                    {/* <img src={require(this.props.displayedResults.overall_img)} class="img-thumbnail" alt="Responsive image"></img>
-                <img src={weblogo}></img> */}
-                    {this.props.displayedResults.overall_img}
-
-                </div>
-                <div class="container" className="result-container-header">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <h5><b className="header5">WCAG AAA:</b> {this.props.displayedResults.wcag_AAA_percentage}</h5>
-                        </div>
-                        <div class="col-sm-4">
-                            <h5><b className="header5">WCAG AA:</b> {this.props.displayedResults.wcag_AA_percentage}</h5>
-                        </div>
-                        <div class="col-sm-4">
-                            <h5><b className="header5">Statistics</b></h5>
-                        </div>
-                    </div>
-                    <div class="row" >
-                        <div class="col-sm-6">
-                            <div class="row">
-                                <div class="col-sm-1"><b>ID</b></div>
-                                <div class="col-sm-5"><b>Color Contrast Ratio</b></div>
-                                <div class="col-sm-3"><b>WCAG AAA</b></div>
-                                <div class="col-sm-3"><b>WCAG AA</b></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-4">
-                            <div> 2 of 2</div>
-                        </div>
-                    </div>
-                    {JSON.stringify(this.props.displayedResults.sub_evaluation[0])}
-                </div>
-            </div>
-        )
+    if (props.displayedResults.sub_evaluation === undefined) {
+        console.log("is loading")
+        return <div><LoadingSpinner /></div>
     }
 
+    return (
+        <div>
+            <h2 className="headline">Evaluation Result</h2>
+            <div class="text-center">
+                {/* <img src={require(this.props.displayedResults.overall_img)} class="img-thumbnail" alt="Responsive image"></img>
+                        <img src={weblogo}></img> */}
+                {/* {props.displayedResults.overall_img} */}
+
+            </div>
+            <div class="container" className="result-container-header">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5><b className="header5">WCAG AAA:</b> {props.displayedResults.wcag_AAA_percentage}
+                            {parseInt(props.displayedResults.wcag_AAA_percentage) < 50
+                                ? <span className="risk-badge"> risk</span>
+                                : <span class="good-badge"> good</span>
+                            }
+                        </h5>
+                    </div>
+                    <div class="col-sm-4">
+                        <h5><b className="header5">WCAG AA:</b> {props.displayedResults.wcag_AA_percentage}
+                            {parseInt(props.displayedResults.wcag_AA_percentage) < 50
+                                ? <span className="risk-badge"> risk</span>
+                                : <span class="good-badge"> good</span>
+                            }
+                        </h5>
+                    </div>
+                    <div class="col-sm-4">
+                        <h5><b className="header5">Statistics</b></h5>
+                    </div>
+                </div>
+                <div class="row" >
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-1"><b className="output-txt">ID</b>
+                                {props.displayedResults.sub_evaluation.map((item, i) => (
+                                    <div key={i} className="output-txt">{item.id}</div>
+                                ))}
+                            </div>
+                            <div class="col-sm-5"><b className="output-txt">Color Contrast Ratio</b>
+                                {props.displayedResults.sub_evaluation.map((item, i) => (
+                                    <div key={i} className="output-txt">{item.ratio}</div>
+                                ))}
+                            </div>
+                            <div class="col-sm-3"><b className="output-txt">WCAG AAA</b>
+                                {props.displayedResults.sub_evaluation.map((item, i) => (
+                                    <div key={i} className="output-txt">
+                                        {item.wcag_AAA === true
+                                            ? <BiCheck className="check-icon" />
+                                            : <RiCloseFill className="cross-icon" />
+                                        }
+                                    </div>
+                                ))}
+                            </div>
+                            <div class="col-sm-3"><b className="output-txt">WCAG AAA</b>
+                                {props.displayedResults.sub_evaluation.map((item, i) => (
+                                    <div key={i} className="output-txt">
+                                        {item.wcag_AA === true
+                                            ? <BiCheck className="check-icon" />
+                                            : <RiCloseFill className="cross-icon" />
+                                        }
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-4">
+                    </div>
+                </div>
+            </div>
+            <ScrollButton/>
+        </div>
+    )
 }
+
