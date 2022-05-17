f86a57daf70f2303e2e91f3f403f00cd0e16b6eb
Rajitha-Manellanga <rajitha.mail48@gmail.com>
2022-05-17 15:19:02 +0300

    [34] Rajitha - Updated text recognition algorithm

--- img_process/process.py
+++ img_process/process.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import detect
 import json
 import pathlib
+import pytesseract
+from pytesseract import Output
 
 class ImageProcess():
 
     def __init__(self, img):
         self.img = img
 
-    def dilate_image(self,img_name):
+    """ def dilate_image(self,img_name):
         img = self.img
         mask = np.ones(img.shape[:2], np.uint8)
         img_gray =  cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -20,7 +22,7 @@ class ImageProcess():
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,15))
         edges = cv2.dilate(edges, rect_kernel, iterations = 1)
         
-        return self.draw_contours(edges,img_name)
+        return self.draw_contours(edges,img_name) """
     
     def draw_contours(self,img,img_name):
         output = {}
@@ -61,7 +63,70 @@ class ImageProcess():
         # Add path to ./react-app/src/pages/contours/  and fix the contour img name (including for returning to response.data) 
         # to be whatever e.g. contour.png
         # so that I can import this image from local on react like import contour from './react-app/src/pages/contours/contour.png' instead
-        cv2.imwrite('./react-app/src/pages/contours/contour.'+pathlib.Path(img_name).suffix,img2)
+        cv2.imwrite('./react-app/src/pages/contours/contour'+pathlib.Path(img_name).suffix,img2)
+
+        output["sub"] = img_list
+        output["img"] = "./contours/"+img_name
+        output["height"] = self.img.shape[0]
+        output["width"] = self.img.shape[1]
+
+        #return json.dumps(output)
+        return output
+
+    def detect_text(self,img_name):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        output = {}
+        num = 0
+        img_list = []
+        img2 = self.img.copy()
+
+        mask = np.ones(self.img.shape[:2], np.uint8)
+        img_gray =  cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.bitwise_and(img_gray,img_gray, mask=mask)
+        img_gray = cv2.GaussianBlur(img_gray,(3,3),cv2.BORDER_DEFAULT)
+
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+        edges = cv2.Canny(img_gray,180,255)
+        edges = cv2.dilate(edges, rect_kernel, iterations = 1)
+        edges = cv2.erode(edges, rect_kernel, iterations = 1)
+
+        results = pytesseract.image_to_data(edges, output_type=Output.DICT,config='--psm 3')
+        escape = [" ","|","*","/","-",".","--","|","»","°","#",".","+"]
+
+        for i in range(0, len(results["level"])):
+
+            text = results["text"][i]
+            conf = int(float(results["conf"][i]))
+
+            if conf != -1 and len(text)>2 and text not in escape:
+                num+=1
+                # extract the bounding box coordinates
+                # of the text region from  the current result
+                x = results["left"][i]
+                y = results["top"][i]
+                w = results["width"][i]
+                h = results["height"][i]
+                ROI = self.img[y:y+h, x:x+w]
+                img_arr = {}
+                img_arr["id"] = num
+                img_arr["width"] = w
+                img_arr["height"] = h
+                cv2.rectangle(img2,(x, y),(x + w, y + h),(0, 0, 255), 2)
+
+                colors = detect.ColorDetector(ROI).detect_colors()
+                colors = sorted(colors)
+                color_1 = colors[0][0]
+                color_2 = colors[-1][0]
+
+                img_arr["color_1"] = color_1
+                img_arr["color_2"] = color_2
+
+                img_arr["is_bold"] = False
+
+                img_list.append(img_arr)
+                cv2.putText(img2,str(num),(x,y-10), font, 1.2,(0,255,0),3)
+
+        cv2.imwrite('./react-app/src/pages/contours/contour'+pathlib.Path(img_name).suffix,img2)
 
         output["sub"] = img_list
         output["img"] = "./contours/"+img_name
